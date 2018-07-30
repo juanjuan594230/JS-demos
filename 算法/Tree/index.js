a12ac8febca36659dd6f7c8b3a4c9963c032d373
renyujuan <363886121@qq.com>
2018-07-30 23:27:25 +0800

    tree

--- 算法/Tree/index.js
+++ 算法/Tree/index.js
@@ -1,8 +1,8 @@
 class Binarytree {
   constructor(value, left, right) {
     this.value = value;
-    this.left = left;
-    this.right = right;
+    this.left = left || null;
+    this.right = right || null;
   }
 }
 
@@ -20,46 +20,55 @@ const A = new Binarytree('1', B, C);
 
 // 递归
 // 先序遍历二叉树
+// function preorder(root) {
+//   if (!root) {
+//     return;
+//   }
+//   console.log(root.value);
+//   if (root.left) {
+//     preorder(root.left);
+//   }
+//   if (root.right) {
+//     preorder(root.right);
+//   }
+// }
+
 function preorder(root) {
   if (!root) {
     return;
   }
   console.log(root.value);
-  if (root.left) {
-    preorder(root.left);
-  }
-  if (root.right) {
-    preorder(root.right);
-  }
+  preorder(root.left);
+  preorder(root.right);
 }
 
 // 中序遍历二叉树
-function inOrder(root) {
-  if (!root) {
-    return;
-  }
-  if (root.left) {
-    inOrder(root.left);
-  }
-  console.log(root.value);
-  if (root.right) {
-    inOrder(root.right);
-  }
-}
+// function inOrder(root) {
+//   if (!root) {
+//     return;
+//   }
+//   if (root.left) {
+//     inOrder(root.left);
+//   }
+//   console.log(root.value);
+//   if (root.right) {
+//     inOrder(root.right);
+//   }
+// }
 
 // 后序遍历二叉树
-function nextOrder(root) {
-  if (!root) {
-    return;
-  }
-  if (root.left) {
-    nextOrder(root.left);
-  }
-  if (root.right) {
-    nextOrder(root.right);
-  }
-  console.log(root.value);
-}
+// function nextOrder(root) {
+//   if (!root) {
+//     return;
+//   }
+//   if (root.left) {
+//     nextOrder(root.left);
+//   }
+//   if (root.right) {
+//     nextOrder(root.right);
+//   }
+//   console.log(root.value);
+// }
 
 // ABDHIEJCFKG
 preorder(A);
@@ -67,6 +76,53 @@ preorder(A);
 // inOrder(A);
 // // HIDJEBKFGCA
 // nextOrder(A);
+
+// 二叉树的高度
+function getTreeHeight(root) {
+  if (!root) {
+    return 0;
+  }
+  const m = getTreeHeight(root.left);
+  const n = getTreeHeight(root.right);
+  return m > n ? m + 1 : n + 1;
+}
+
+console.log(getTreeHeight(A)); 
+console.log(A);
+
+// 复制二叉树
+function deepClone(root, obj = {}) {
+  for (let attr in root) {
+    if (root.hasOwnProperty(attr)) {
+      if (typeof root[attr] === 'object' && root[attr]) {
+        obj[attr] = {};
+        deepClone(root[attr], obj[attr]);
+      } else {
+        obj[attr] = root[attr];
+      }
+    }
+  }
+  return obj;
+}
+const obj = deepClone(A, {});
+// console.log(obj);
+// preorder(obj);
+
+function clone(root, newRoot) {
+  if (!root) {
+    newRoot = null;
+    return;
+  }
+  newRoot.value = root.value;
+  newRoot.left = root.left ? new Binarytree() : null;
+  newRoot.right = root.right ? new Binarytree() : null;
+  clone(root.left, newRoot.left);
+  clone(root.right, newRoot.right);
+}
+
+let _A = new Binarytree();
+clone(A, _A);
+console.log(_A);
 
 /* 二叉树上寻找节点第K小的数 */
 
